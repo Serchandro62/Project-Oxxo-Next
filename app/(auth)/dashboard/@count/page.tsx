e7b8c176064817ca1198ba4ede7c861f40bfbff0
Serchandro62 <sergio.angel.lopez.arteaga@gmail.com>
2025-10-05 15:52:40 -0600

    feat: cookies in ssc

--- app/(auth)/dashboard/@count/page.tsx
+++ app/(auth)/dashboard/@count/page.tsx
@@ -1,8 +1,16 @@
 import axios from "axios";
+import { cookies, headers } from "next/headers";
+import { TOKEN_NAME } from "@/constants";
 
 const CountPage = async () => {
-   const countLocations = await axios.get("http://localhost:4000/locations");
-   console.log(countLocations.data);
-   return "Hay tantas locatons: " + countLocations?.data?.length;
+   const userCookies = cookies();
+   const token = (await userCookies).get(TOKEN_NAME)?.value;
+   const countLocations = await axios.get("http://localhost:4000/locations", {
+      headers: {
+         Authorization: `Bearer ${token}`
+      }
+   });
+   const cantidad = countLocations?.data?.length
+   return `Hay: ${cantidad} tienda${cantidad > 1 ? 's' : ""}`;
 }
 export default CountPage;
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,22 +1,32 @@
 'use client'
 import { API_URL } from "@/constants";
-import { Button, Input } from "@heroui/react";
+import { Button, Input, Spinner } from "@heroui/react";
 import axios from "axios";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
-export default function LoginPage(){
+export default function LoginPage() {
+    const [submitting, setSubmitting] = useState(false)
+    const router = useRouter();
     const handleSubmit = async (e: React.FormEvent) => {
+        setSubmitting(true);
         e.preventDefault();
         const formData = new FormData(e.target as any);
         let authData: any = {}
         authData.userEmail = formData.get("userEmail");
-        authData.userPassword = formData.get("userPassword"); 
-        const {data} = await axios.post(`${API_URL}/users/login`,{
-            ...authData
-        },{
-            withCredentials:true
-        }); 
-        console.log(data);
+        authData.userPassword = formData.get("userPassword");
+        try {
+            const response = await axios.post(`${API_URL}/users/login`, {
+                ...authData
+            }, {
+                withCredentials: true
+            });
+            if(response.status === 201) router.push('/dashboard')
+            setSubmitting(false);
+        } catch (e) {
+            setSubmitting(false);
+        }
         return;
     }
     return (
@@ -27,9 +37,9 @@ export default function LoginPage(){
                     <Input name="userEmail" className="bg-white rounded overflow-hidden px-1 py-2" placeholder="Email" type="email" isRequired={true} size="sm" />
                     <Input name="userPassword" className="bg-white rounded overflow-hidden px-1 py-2" placeholder="Contraseña" type="password" isRequired={true} size="sm" />
                 </div>
-                
+
                 <div className="flex flex-col items-center gap-2">
-                    <Button type="submit" color="primary" className="bg-blue-700 rounded px-5 py-2 text-white">Iniciar Sesión</Button>
+                    <Button disabled={submitting} type="submit" color="primary" className="bg-blue-700 rounded px-5 py-2 text-white">{submitting ? "Enviando..." : "Iniciar Sesión"}</Button>
                     <p>¿No tienes cuenta? <Link href="./signup" className="text-white underline">Registrate</Link></p>
                 </div>
             </form>
